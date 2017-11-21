7c3f9873f5eeacd0be41a6b541a4cfdd154fdfe5
jh2633 <joseph.jr@gmail.com>
2017-11-21 19:28:03 +0000

    pass first test

---
+++ lib/bitmap.rb
@@ -0,0 +1,6 @@
+class Bitmap
+
+    def i(x, y)
+      matrix = Array.new(y) {Array.new(x, "O")}
+    end
+end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require '../lib/bitmap.rb'
+require_relative '../lib/bitmap.rb'
 
 describe Bitmap do
   before(:each) do
@@ -8,8 +8,8 @@ describe Bitmap do
 
   describe 'command I' do
     it 'takes two arguments to mulitply and create a matrix with elements "O"' do
-      two_by_two = [["O","O"]["O","O"]]
-      expect (@bitmap.i(2,2)).to eq(two_by_two)
+      three_by_two = [["O","O","O"],["O","O","O"]]
+      expect(@bitmap.i(3,2)).to eq(three_by_two)
     end
   end
 end
