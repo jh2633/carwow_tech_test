13c0af7d9bac9dbfee46ed05ed8f568a2d5fc929
jh2633 <joseph.jr@gmail.com>
2017-11-21 19:49:30 +0000

    add method to change a specific x,y pixel

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,6 +1,19 @@
 class Bitmap
+attr_accessor :matrix
+
+    def c
+      @matrix.each do |x|
+        x.map! {|c| c = "O" }
+      end
+    end
 
     def i(x, y)
       matrix = Array.new(y) {Array.new(x, "O")}
+    end
+
+    def l(x,y,c)
+      x -= 1
+      y -= 1
+      @matrix[x][y] = c
     end
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -4,12 +4,31 @@ require_relative '../lib/bitmap.rb'
 describe Bitmap do
   before(:each) do
     @bitmap = Bitmap.new
+    @bitmap.matrix = [["O","O","O"],["O","O","O"]]
+
   end
 
   describe 'command I' do
     it 'takes two arguments to mulitply and create a matrix with elements "O"' do
       three_by_two = [["O","O","O"],["O","O","O"]]
       expect(@bitmap.i(3,2)).to eq(three_by_two)
+    end
+  end
+
+  describe 'command C' do
+    it 'clears the matrix, changing all elements to "O" ' do
+      three_by_two =
+      @bitmap.matrix = [["C","C","C"],["C","C","C"]]
+      @bitmap.c
+      expect(@bitmap.matrix).to eq(three_by_two)
+    end
+  end
+
+  describe 'command L' do
+    it 'colours a pixel with a designated colour' do
+      color_three_by_two = [["C","O","O"],["O","O","O"]]
+      @bitmap.l(1,1,"C")
+      expect(@bitmap.matrix).to eq(color_three_by_two)
     end
   end
 end
